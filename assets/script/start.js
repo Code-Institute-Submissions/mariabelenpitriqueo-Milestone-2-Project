6dd1ac3fd90d5e2b7f1ae6f863772c32ebe1fa68
mariabelenpitriqueo <mb.pitriqueo@gmail.com>
2021-01-06 01:46:54 +0000

    functions and separation of actions through comments for a better organization

--- assets/script/start.js
+++ assets/script/start.js
@@ -1,6 +1,12 @@
-var cardsgroup = ["🦸", "️👾" ,"👽", "⚡", "💥", "⭐", "🎯", "💣", "🧨", "🛡️"];
 
+/*VARIABLES GLOBALES*/ 
+var cardsgroup = ["🦸", "️👾" ,"👽", "⚡", "💥", "⭐", "🎯", "💣", "🧨", "🛡️"];
 var allcards = cardsgroup.concat(cardsgroup);
+
+
+
+
+/*BARAJA REPARTE */ 
 
 function shuffledcards() {
 var result;
@@ -36,8 +42,13 @@ function dealcards() {
 
 }
 
+
+/*DESCUBRIR Y COMPARAR*/ 
+
+
 function show (){
     var showed;
+    var tarjetasPendientes;
 
     var allshowed = document.querySelectorAll(".uncovered:not(.accurate)");
 
@@ -55,6 +66,11 @@ function show (){
     
      
     compare(showed);
+
+    tarjetasPendientes= document.querySelectorAll(".card:not(.accurate)");
+    if (tarjetasPendientes.length === 0){
+       setTimeout(finalizar, 1000);
+    } 
 }
 
 
@@ -69,6 +85,9 @@ function compare(cardsToCompare) {
   }
 }
 
+
+
+/*ACIERTO Y ERROR*/ 
 
 function success(theCards) {
   theCards.forEach(function(element) {
@@ -90,11 +109,68 @@ function error(theCards) {
 }
 
 
+
+/*-INICIAR -*/
+function iniciar (){
+
 dealcards();
+
+document.querySelector("#feedback").classList.remove("visible");
 
 document.querySelectorAll(".card").forEach(function(element) {
     element.addEventListener("click", show);
+    });
+    iniciaCronometro();
+}
+
+iniciar();
 
 
+document.querySelector("#reiniciar").addEventListener("click",iniciar);
+
+
+function finalizar() {
+document.querySelector("#feedback").classList.add("visible");
+}
+
+
+
+
+
+
+
+/* Contador */
+function iniciaCronometro() {
+  var segundos = 10;
+  var minutos = 0;
+  var segundosTexto;
+  var minutosTexto;
+  var cronometro;
+
+  function actualizaContador() {
+    segundos--;
+    if (segundos < 0) {
+      segundos = 59;
+      minutos--;
     }
-);
+    if (minutos < 0) {
+      segundos = 0;
+      minutos = 0;
+      clearInterval(cronometro);
+      
+    }
+    segundosTexto = segundos;
+    minutosTexto = minutos;
+    if (segundos < 10) {
+      segundosTexto = "0" + segundos;
+    }
+    if (minutos < 10) {
+      minutosTexto = "0" + minutos;
+    }
+    document.querySelector("#minutos").innerText = minutosTexto;
+    document.querySelector("#segundos").innerText = segundosTexto;
+  }
+  cronometro = setInterval(actualizaContador, 1000);
+}
+
+
