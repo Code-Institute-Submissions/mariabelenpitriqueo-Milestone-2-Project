a7af3ffc9a69996fbe9bfa373df9debc7a2becf7
mariabelenpitriqueo <mb.pitriqueo@gmail.com>
2021-02-17 12:10:34 +0000

    file created and fuctions pasted for a better organization

---
+++ assets/script/timer.js
@@ -0,0 +1,41 @@
+/* COUNTER */
+     
+     function startClock() {
+    
+     let seconds = 35;
+     let minutes = 0;
+     let secondsText;
+     let minutesText;
+     let clock;
+  
+     function actualizaContador() {
+      
+
+     seconds--;
+     if (seconds < 0) {
+      seconds = 59;
+      minutes--;
+    }
+    
+     if (minutes < 0) {
+      seconds = 0;
+      minutes = 0;
+      clearInterval(clock);
+      modal.style.display = "flex";
+      
+    }
+     secondsText = seconds;
+     minutesText = minutes;
+     if (seconds < 10) {
+      secondsText = "0" + seconds;
+    }
+     if (minutes < 10) {
+      minutesText = "0" + minutes;
+    }
+
+     document.querySelector("#minutes").innerText = minutesText;
+     document.querySelector("#seconds").innerText = secondsText;
+  }
+    clock = setInterval(actualizaContador, 1200);
+  
+ }
