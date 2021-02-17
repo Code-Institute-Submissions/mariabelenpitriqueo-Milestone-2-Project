59420016fafb43b7d6698e7966e1ff0135180daa
mariabelenpitriqueo <mb.pitriqueo@gmail.com>
2021-02-17 22:37:41 +0000

    Add jshint esversion: 6 comment

--- assets/script/variables.js
+++ assets/script/variables.js
@@ -1,4 +1,6 @@
 /*GLOBAL VARIABLES*/ 
+/*jshint esversion: 6 */
+
      let cardsgroup = ["🦸", "️👾" ,"👽", "⚡", "💥", "⭐", "🎯", "💣"];
      let allcards = cardsgroup.concat(cardsgroup);
      const modal = document.getElementById('modal');
